a81e31bcccbb3b55cd15a7bb0bf6b04f15d47eed
Thiago <4077420+thiagovf@users.noreply.github.com>
2021-11-05 11:29:24 -0300

    Adicionando topico controller

---
+++ src/main/java/dev/thiagofernandes/forum/controller/TopicoController.java
@@ -0,0 +1,23 @@
+package dev.thiagofernandes.forum.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import dev.thiagofernandes.forum.modelo.Curso;
+import dev.thiagofernandes.forum.modelo.Topico;
+
+@Controller
+public class TopicoController {
+
+	@RequestMapping(value = "/topicos")
+	@ResponseBody
+	public List<Topico> lista() {
+		Topico topico = new Topico("Titulo", "Mensagem", new Curso("SpringBoot", "Programação"));
+		
+		return Arrays.asList(topico, topico, topico);
+	}
+}
