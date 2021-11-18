f234e4ad5ef9c02ab2842c8df2f8de5070776b26
Thiago <4077420+thiagovf@users.noreply.github.com>
2021-11-18 11:01:40 -0300

    Adicionando validação dos parâmetros passados na requisição

--- src/main/java/dev/thiagofernandes/forum/controller/form/TopicoForm.java
+++ src/main/java/dev/thiagofernandes/forum/controller/form/TopicoForm.java
@@ -1,13 +1,28 @@
 package dev.thiagofernandes.forum.controller.form;
 
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 import dev.thiagofernandes.forum.modelo.Curso;
 import dev.thiagofernandes.forum.modelo.Topico;
 import dev.thiagofernandes.forum.repository.CursoRepository;
 
 public class TopicoForm {
 
+	@NotNull
+	@NotEmpty
+	@Size(min = 5)
 	private String titulo;
+	
+	@NotNull
+	@NotEmpty
+	@Size(min = 10)
 	private String mensagem;
+	
+	@NotNull
+	@NotEmpty
 	private String nomeCurso;
 
 	public String getTitulo() {
